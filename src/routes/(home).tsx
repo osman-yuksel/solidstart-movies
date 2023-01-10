8193e94dd15419a8877197b80daf19e78a5b1376
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-11 01:25:05 +0300

    vercel adapter disabled.

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -20,7 +20,6 @@ export function routeData({ params }: RouteDataArgs) {
 
 export default function Home() {
   const popularMovies = useRouteData<typeof routeData>();
-  console.log(import.meta.env.VITE_HOSTNAME, import.meta.env.VITE_TMDB_API_KEY);
   return (
     <main class="mx-auto p-4 text-center text-gray-700">
       <h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">
